9a4a332e88c377aa6f6450277ef275e59810b0c9
Spinozist <ewoodworth@atlantaregional.org>
2018-11-04 12:32:52 -0500

    customer and admin functions working

--- storefront.js
+++ storefront.js
@@ -12,32 +12,199 @@ var connection = mysql.createConnection({
 
     // Your password
     password: "",
-    database: "inventory"
+    database: "storefront_db"
 });
 
 connection.connect(function (err) {
     if (err) throw err;
-    showInventory();
-    enterStore();
+    role();
 });
 
-enterStore();
+
+function showInventory() {
+    console.log(`***THIS IS WHAT WE'VE GOT***`)
+    console.log(`id || item || price || stock`)
+    var query = "SELECT * FROM inventory";
+    connection.query(query, function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(` ${res[i].id} // ${res[i].name} // ${res[i].price} // ${res[i].in_stock}`);
+        }
+        if (res) {
+            enterStore();
+        }
+    });
+};
+
+function role() {
+    inquirer
+        .prompt(
+            {
+                name: "role",
+                type: "list",
+                message: "Who are you?",
+                choices: [
+                    "Customer",
+                    "Admin"
+                ]
+            }
+        )
+        .then(function (answer) {
+            switch (answer.role) {
+                case "Customer":
+                    showInventory();
+                    break;
+
+                case "Admin":
+                    passWord();
+                    break;
+            }
+        })
+}
 
 function enterStore() {
+
     inquirer
-        .prompt([
+        .prompt(
             {
-                name: "itemID",
-                type: "input",
-                message: "Item ID?",
-            },
-            {
-                name: "itemQuantity",
-                type: "input",
-                message: "How many?",
+                name: "buy",
+                type: "list",
+                message: "Wanna buy something?",
+                choices: [
+                    "Yes",
+                    "No"
+                ]
             }
-        ])
+        )
         .then(function (answer) {
-            console.log(`You'd like ${answer.itemQuantity} of item ${answer.itemID}`);
+            if (answer.buy === "Yes") {
+                inquirer
+                    .prompt([
+                        {
+                            name: "itemID",
+                            type: "input",
+                            message: "Item ID?",
+                        },
+                        {
+                            name: "itemQuantity",
+                            type: "input",
+                            message: "How many?",
+                        }
+                    ])
+                    .then(function (answer) {
+                        console.log(`You'd like ${answer.itemQuantity} of item ${answer.itemID}`);
+                        buyItem(answer.itemID, answer.itemQuantity);
+                        logPurchases(answer.itemID, answer.itemQuantity)
+                    })
+            } else {
+                restartPrompt();
+            }
+        })
+};
+
+function buyItem(itemID, amount) {
+    connection.query(
+        `UPDATE inventory SET in_stock = in_stock - ${amount} WHERE id=${itemID};`,
+        function (error) {
+            if (error) throw err;
+            if (amount > 1) {
+                console.log('Items purchased successfully!');
+            } else {
+                console.log('Item purchased successfully!');
+            }
+            showInventory();
+        }
+    )
+}
+
+function logPurchases(itemID, amount) {
+    connection.query(
+        `INSERT INTO sales (item_id,quantity) VALUES (${itemID},${amount});`,
+        function () {
+            // if (error) throw err;
+            // console.log(`Sale logged`);
+        }
+    )
+}
+
+function passWord() {
+    inquirer
+        .prompt(
+            {
+                name: "password",
+                type: "input",
+                messages: "Enter password"
+            }
+        ).
+        then(function (answer) {
+            if (answer.password === "r2d2") {
+                showSales();
+            } else {
+                console.log(`Accessed denied!`);
+                restartPrompt();
+            }
+        })
+}
+
+
+function restartPrompt() {
+    inquirer
+        .prompt({
+            name: "restart",
+            type: "list",
+            message: "Restart?",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        })
+        .then(function (answer) {
+            if (answer.restart === "Yes") {
+                role();
+            } else {
+                console.log(`Press ^C to Exit.`);
+            }
+        })
+}
+
+// var query = "SELECT top5000albums.year, top5000albums.album, top5000albums.position, top5000songs.song, top5000songs.artist ";
+//   query += "FROM top5000albums INNER JOIN top5000songs ON (top5000albums.artist = top5000songs.artist AND top5000albums.year ";
+//   query += "= top5000songs.year) WHERE (top5000albums.artist = ? AND top5000songs.artist = ?) ORDER BY top5000albums.year "
+
+
+function showSales() {
+    console.log(`******SALES******`);
+    console.log(`id // item // #`);
+    var query = `
+    SELECT item_id, id, quantity, name, price, department 
+    FROM sales 
+    LEFT JOIN inventory ON sales.item_id = inventory.id;
+    `;
+    connection.query(query, function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(`${res[i].item_id} // ${res[i].department} // ${res[i].name} // ${res[i].quantity} @ ${res[i].price} = $${res[i].quantity * res[i].price}`);
+        }
+        departmentSales();
+    });
+};
+
+function departmentSales() {
+    inquirer
+        .prompt({
+            name: "departmentSales",
+            type: "list",
+            message: "Wanna see department totals?",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        })
+        .then(function (answer) {
+            if (answer.departmentSales === "Yes") {
+                console.log(`  DEPARTMENT SALES**`);
+                console.log(`  Dept || Total Sales`)
+                //Function to show department totals
+
+            }
+            restartPrompt();
         })
 }
--- storefront.sql
+++ storefront.sql
@@ -7,12 +7,30 @@ create table inventory (
     department varchar (20) not null, 
     name varchar (20) not null,
     price decimal (10,2) not null default 0,
-    quantity decimal (10,0) not null default 1,
+    in_stock decimal (10,0) not null default 1,
     primary key (id)
 );
 
+create table sales (
+	sale_id int (5) not null auto_increment,
+    item_id int (5) not null,
+    quantity decimal (10,0) not null,
+    primary key (sale_id)
+);
 
--- insert into inventory (name,price,quantity)
--- 	values ("collage",300, 1);
+
+insert into inventory (name,price,in_stock,department)
+	values 
+    ("collage",300, 1,"art"),
+    ("cloud maker", 175, 10, "science"),
+    ("ability of flight", 800, 5,"magic");
+    
+
+    
     
 select * from inventory;
+select * from sales;
+
+SELECT item_id, id, quantity, name 
+FROM sales 
+LEFT JOIN inventory ON sales.item_id = inventory.id;
