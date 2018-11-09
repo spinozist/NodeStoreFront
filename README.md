b766a0a2e1c2220353bef8c95c47cc647ce1d31f
Spinozist <ewoodworth@atlantaregional.org>
2018-11-09 18:29:56 -0500

    added items to inventory

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # NodeStoreFront
+
+## User Descritption
+CLI application which allows a user to interact with the store inventory data as either a customer or an admin.  Customers can view the store inventory (with columns for the item id, department, description, price, and nubmer of items in stock) and then buy a given number of an items by entering the item id and desired quantity.  The inventory veiw is then refreshed to show the items in stock after the purchase.  Admin, after entering a password, can view a log sales.  An extended version of this application would allow the admin to view sales totals aggregated by department.
--- storefront.sql
+++ storefront.sql
@@ -5,7 +5,7 @@ use storefront_db;
 create table inventory (
 	id int (5) not null auto_increment,
     department varchar (20) not null, 
-    name varchar (20) not null,
+    name varchar (50) not null,
     price decimal (10,2) not null default 0,
     in_stock decimal (10,0) not null default 1,
     primary key (id)
@@ -15,26 +15,35 @@ create table sales (
 	sale_id int (5) not null auto_increment,
     item_id int (5) not null,
     quantity decimal (10,0) not null,
-    saleTotal decimal(10,2) not null,
     primary key (sale_id)
 );
 
-create table salesTotal (
-	department varchar (20) not null,
-    total_num int (5) not null default 0,
-    total_sales decimal (10,2) not null default 0
-);
+-- create table salesTotal (
+-- 	department varchar (20) not null,
+--     total_num int (5) not null default 0,
+--     total_sales decimal (10,2) not null default 0
+-- );
 
 
 insert into inventory (name,price,in_stock,department)
 	values 
     ("collage",300, 1,"art"),
     ("cloud maker", 175, 10, "science"),
-    ("ability of flight", 800, 5,"magic");
+    ("ability of flight", 800, 5,"magic"),
+    ("smell remover",50, 32,"science"),
+	("used car mobile", 1450, 4, "art"),
+    ("long hair potion",25 ,112 ,"magic"),
+    ("hole digger",60 ,400 ,"things"),
+    ("round ball",5 ,1350 ,"things"),
+    ("x-ray specs",250 ,75 ,"science"),
+    ("universal knowlegde",0 ,0 ,"magic"),
+    ("futurist sound sculpture",450 ,15 ,"art"),
+    ("funny hat",4 ,5000 ,"things");
+
     
 
     
     
 select * from inventory;
 select * from sales;
-select * from salesTotal;
+-- select * from salesTotal;
